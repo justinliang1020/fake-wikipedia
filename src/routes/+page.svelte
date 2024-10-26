f04f98839aa619b27fcfcbb277515730c90238b3
Justin Liang <justinliang1020@gmail.com>
2024-10-26 18:19:20 -0400

    add textarea for caption

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script>
 	import ImageFigure from '$lib/ImageFigure.svelte';
+
+	let caption = $state(
+		'Skulls of a wildcat (top left), a housecat (top right), and a hybrid between the two (bottom center)'
+	);
 </script>
 
-<ImageFigure
-	caption="Skulls of a wildcat (top left), a housecat (top right), and a hybrid between the two (bottom center)"
-	imageSrc="catskull.png"
-/>
+<ImageFigure {caption} imageSrc="catskull.png" />
+
+<form>
+	<label for="caption">caption</label>
+	<textarea bind:value={caption} id="caption" rows="7" cols="35"></textarea>
+</form>
