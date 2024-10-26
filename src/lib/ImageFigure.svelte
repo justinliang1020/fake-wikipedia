fda7f9f1a28292fc75e1c11a84fdff60f1c6bf14
Justin Liang <justinliang1020@gmail.com>
2024-10-26 17:52:04 -0400

    make ImageFigure component

---
+++ src/lib/ImageFigure.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	let { caption, imageSrc }: { caption: string; imageSrc: string } = $props();
+</script>
+
+<figure>
+	<img src={imageSrc} alt="meow" />
+	<figcaption>
+		{caption}
+	</figcaption>
+</figure>
+
+<style>
+	figure {
+		width: 229px;
+		background-color: rgb(248, 249, 250);
+		border: 1px solid #c8ccd1;
+		border-bottom: 0;
+		font-family: sans-serif;
+		display: table;
+	}
+
+	img {
+		max-width: calc(100% - (2 * 3px) - (2 * 1px));
+		border: 1px solid #c8ccd1;
+		margin: 3px;
+	}
+
+	figcaption {
+		display: table-caption;
+		caption-side: bottom;
+		line-height: 1.4em;
+		padding: 0 6px 6px 6px;
+		border: 1px solid #c8ccd1;
+		border-top: 0;
+		background-color: #f8f9fa;
+		font-size: 88%;
+		background-image: url('https://en.wikipedia.org/w/resources/src/mediawiki.skinning/images/magnify-clip-ltr.svg?8330e');
+		background-repeat: no-repeat;
+		background-position: top right 5px;
+	}
+</style>
