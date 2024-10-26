7e3df4b7c24439e23bd6c886672f51cbf9223cb2
Justin Liang <justinliang1020@gmail.com>
2024-10-26 17:27:06 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<figure>
+	<img src="catskull.png" alt="meow" />
+	<figcaption>
+		Skulls of a wildcat (top left), a housecat (top right), and a hybrid between the two (bottom
+		center)
+	</figcaption>
+</figure>
+
+<style>
+	figure {
+		width: 229px;
+		background-color: rgb(248, 249, 250);
+		border: 1px solid #c8ccd1;
+		border-bottom: 0;
+		font-family: sans-serif;
+		display: table;
+	}
+
+	img {
+		max-width: calc(100% - (2 * 3px) - (2 * 1px));
+		border: 1px solid #c8ccd1;
+		margin: 3px;
+	}
+
+	figcaption {
+		display: table-caption;
+		caption-side: bottom;
+		line-height: 1.4em;
+		padding: 0 6px 6px 6px;
+		border: 1px solid #c8ccd1;
+		border-top: 0;
+		background-color: #f8f9fa;
+		font-size: 88%;
+	}
+</style>
