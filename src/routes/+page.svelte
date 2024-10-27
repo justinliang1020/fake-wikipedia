038d907011ce8df946c2e8185c9f336dfe33822e
Justin Liang <justinliang1020@gmail.com>
2024-10-27 10:08:09 -0400

    bruh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,16 @@
 	let imageSrc = $state('catskull.png');
 </script>
 
+<h1>Fake Wikipedia Image Generator</h1>
 <ImageFigure {caption} {imageSrc} />
+
+<br />
+<br />
+
 <label for="caption">Caption (raw HTML)</label>
 <textarea bind:value={caption} id="caption" rows="7" cols="35"></textarea>
 
 <br />
 
 <label for="imageUrl">Image URL</label>
-<input type="text" id="imageUrl" name="Image URL" bind:value={imageSrc} />
+<input type="text" id="imageUrl" name="Image URL" bind:value={imageSrc} style="width: 400px;" />
